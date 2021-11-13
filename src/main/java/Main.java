b1cd8fd8465dd37322a47a2c07e0e823d0262969
akasyuka <aleksandrkasyuk@gmail.com>
2021-11-13 13:33:12 +0500

    Нажатие и ввода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +15,15 @@ public class Main {
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.google.com");
         WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
-        //WebElement input = driver.findElements(By.xpath("//input[@aria-label='Найти']"));
-        //input.click();
-        input.sendKeys("КБК фиксированного платежа ", Keys.ENTER);
+        //найти неск. элементов
+        //WebElement input = driver.findElements(By.xpath("//input[@aria-label='Найти']"));найти неск. эл.
+        input.click();//нажать на область
+        input.sendKeys("КБК фиксированного платежа ", Keys.ENTER);//ввести в поиск ...
+        input = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/a/h3"));
+        input.click();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presense);
+
     }
 }
